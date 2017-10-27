f68e601492ec762b66cdcdf7b6aca0d0f8a5961f
mengguiyouziyi <775618369@qq.com>
2017-10-27 21:15:35 +0800

    1

--- use/get_zhuanli_wai.py
+++ use/get_zhuanli_wai.py
@@ -28,25 +28,54 @@ try:
 	results = etl_cur.fetchall()
 
 	m = 0
-	for result in results:
-		for i in range(1, 12):
-			sql_1 = """select * from patent_{0} WHERE applicantName LIKE '{1}'""".format(str(i), (
-			'%' + result['comp_full_name'] + '%'))
-			# print(sql_1)
-			etl_cur.execute(sql_1)
-			results_1 = etl_cur.fetchall()
-			m += len(results_1)
-			print(m)
-			sql_2 = """insert into zhuanli_patent_wai (pid,appNumber,pubNumber,appDate,pubDate,title,ipc,applicantName,inventroName,family,agencyName,agentName,addrProvince,addrCity,addrCounty,address,patType,abs,lprs,draws,dbName,tifDistributePath,pages,proCode,appCoun,gazettePath,gazettePage,gazetteCount,statusCode,familyNo,legalStatus,mainIpc,appResource,cl,patentWords,page) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-			l = ['pid', 'appNumber', 'pubNumber', 'appDate', 'pubDate', 'title', 'ipc', 'applicantName', 'inventroName',
-			     'family', 'agencyName', 'agentName', 'addrProvince', 'addrCity', 'addrCounty', 'address', 'patType',
-			     'abs', 'lprs', 'draws', 'dbName', 'tifDistributePath', 'pages', 'proCode', 'appCoun', 'gazettePath',
-			     'gazettePage', 'gazetteCount', 'statusCode', 'familyNo', 'legalStatus', 'mainIpc', 'appResource', 'cl',
-			     'patentWords', 'page']
-			values = [[record[i] for i in l] for record in results_1]
-			etl_cur.executemany(sql_2, values)
-			etl.commit()
+	for i in range(1, 12):
+		comp_full_name = [result['comp_full_name'] for result in results]
+		sql_1 = """select * from patent_{0} WHERE applicantName in ({1})""".format(str(i), comp_full_name)
+		# sql_1 = """select * from patent_{0} WHERE applicantName LIKE '{1}'""".format(str(i), ('%' + result['comp_full_name'] + '%'))
+		# print(sql_1)
+		etl_cur.execute(sql_1)
+		results_1 = etl_cur.fetchall()
+		m += len(results_1)
+		print(m)
+		sql_2 = """insert into zhuanli_patent_wai (pid,appNumber,pubNumber,appDate,pubDate,title,ipc,applicantName,inventroName,family,agencyName,agentName,addrProvince,addrCity,addrCounty,address,patType,abs,lprs,draws,dbName,tifDistributePath,pages,proCode,appCoun,gazettePath,gazettePage,gazetteCount,statusCode,familyNo,legalStatus,mainIpc,appResource,cl,patentWords,page) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+		l = ['pid', 'appNumber', 'pubNumber', 'appDate', 'pubDate', 'title', 'ipc', 'applicantName', 'inventroName',
+		     'family', 'agencyName', 'agentName', 'addrProvince', 'addrCity', 'addrCounty', 'address', 'patType',
+		     'abs', 'lprs', 'draws', 'dbName', 'tifDistributePath', 'pages', 'proCode', 'appCoun', 'gazettePath',
+		     'gazettePage', 'gazetteCount', 'statusCode', 'familyNo', 'legalStatus', 'mainIpc', 'appResource', 'cl',
+		     'patentWords', 'page']
+		values = [[record[i] for i in l] for record in results_1]
+		etl_cur.executemany(sql_2, values)
+		etl.commit()
 except:
 	traceback.print_exc()
 finally:
 	etl.close()
+
+# try:
+# 	sql = """select comp_full_name from zhuanli_wai_comp"""
+# 	etl_cur.execute(sql)
+# 	results = etl_cur.fetchall()
+#
+# 	m = 0
+# 	for result in results:
+# 		for i in range(1, 12):
+# 			sql_1 = """select * from patent_{0} WHERE applicantName LIKE '{1}'""".format(str(i), ('%' + result['comp_full_name'] + '%'))
+# 			# sql_1 = """select * from patent_{0} WHERE applicantName LIKE '{1}'""".format(str(i), ('%' + result['comp_full_name'] + '%'))
+# 			# print(sql_1)
+# 			etl_cur.execute(sql_1)
+# 			results_1 = etl_cur.fetchall()
+# 			m += len(results_1)
+# 			print(m)
+# 			sql_2 = """insert into zhuanli_patent_wai (pid,appNumber,pubNumber,appDate,pubDate,title,ipc,applicantName,inventroName,family,agencyName,agentName,addrProvince,addrCity,addrCounty,address,patType,abs,lprs,draws,dbName,tifDistributePath,pages,proCode,appCoun,gazettePath,gazettePage,gazetteCount,statusCode,familyNo,legalStatus,mainIpc,appResource,cl,patentWords,page) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+# 			l = ['pid', 'appNumber', 'pubNumber', 'appDate', 'pubDate', 'title', 'ipc', 'applicantName', 'inventroName',
+# 			     'family', 'agencyName', 'agentName', 'addrProvince', 'addrCity', 'addrCounty', 'address', 'patType',
+# 			     'abs', 'lprs', 'draws', 'dbName', 'tifDistributePath', 'pages', 'proCode', 'appCoun', 'gazettePath',
+# 			     'gazettePage', 'gazetteCount', 'statusCode', 'familyNo', 'legalStatus', 'mainIpc', 'appResource', 'cl',
+# 			     'patentWords', 'page']
+# 			values = [[record[i] for i in l] for record in results_1]
+# 			etl_cur.executemany(sql_2, values)
+# 			etl.commit()
+# except:
+# 	traceback.print_exc()
+# finally:
+# 	etl.close()
