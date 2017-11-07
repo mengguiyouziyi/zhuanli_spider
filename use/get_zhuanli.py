312d32ba0fb9cabf309042df730fac0fd3be54d1
mengguiyouziyi <775618369@qq.com>
2017-11-07 13:17:44 +0800

    1

--- use/get_zhuanli.py
+++ use/get_zhuanli.py
@@ -147,7 +147,9 @@ def get_res(token, result, page):
 		return
 	elif not context:
 		# 说明接口调用超过限制
+		# 有别的可能
 		print(id, '~~no context~~', page, datetime.now())
+		print(info.strip())
 		print('program over...')
 		sys.exit(1)
 	elif errorCode == '000000':
@@ -220,7 +222,7 @@ def main():
 		# unknowerror:1083
 		# Syntax error:3492,4729 {'access_token': '1723a76d-45fe-4b0f-8d3a-67473a1d3314', 'express': '申请人=卫材R&D管理有限公司', 'page': '1', 'client_id': '6050f8adac110002270d833aed28242d', 'page_row': '100', 'scope': 'rea     d_cn'}
 		# 51731～～～～44398 行
-		if id <= 62024:
+		if id <= 63540:
 			continue
 		response = get_res(token, result, 1)
 		if response == -1:
