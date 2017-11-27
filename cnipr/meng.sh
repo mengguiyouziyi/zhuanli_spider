9a2b6b05ac193b36ed3abd98ddbbb777634d00fa
mengguiyouziyi <775618369@qq.com>
2017-11-27 14:32:41 +0800

    1

--- cnipr/meng.sh
+++ cnipr/meng.sh
@@ -19,8 +19,9 @@ do
         #IDD=`ps -ef | grep "cmd_meng.py" |grep -v "grep"| awk '{print $2}'`
         #echo "programing $IDD"
         echo "killed $id"
+    echo "sleep 10s..."
     sleep 10
     done
 done
 
-nohup python /data1/spider/menggui/zhuanli_spider/cnipr/cmd_meng.py >> /data1/spider/menggui/zhuanli_spider/cnipr/meng.out 2>&1 &
+nohup python /data1/spider/menggui/zhuanli_spider/cnipr/cmd_meng.py >> /data1/spider/menggui/zhuanli_spider/cnipr/meng_1.out 2>&1 &
