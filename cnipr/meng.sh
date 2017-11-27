13b489c12479c14c811547bcf98e84efe8e0353f
mengguiyouziyi <775618369@qq.com>
2017-11-27 14:56:46 +0800

    1

--- cnipr/meng.sh
+++ cnipr/meng.sh
@@ -6,23 +6,29 @@ eval "$(pyenv init -)"
 pyenv deactivate
 pyenv activate env354
 
+ID=`ps -ef | grep "cmd_meng.py" |grep -v "grep"| awk '{print $2}'`
+echo "first time $ID"
+for id in $ID
+do
+    kill $id
+    #IDD=`ps -ef | grep "cmd_meng.py" |grep -v "grep"| awk '{print $2}'`
+    #echo "programing $IDD"
+    echo "killed $id"
+done
+echo "first sleep 10s..."
+sleep 10
+
 while true
 do
-    ID=`ps -ef | grep "cmd_meng.py" |grep -v "grep"| awk '{print $2}'`
-    echo "start $ID"
-    if [ ! "$ID" ]
+    IDD=`ps -ef | grep "cmd_meng.py" |grep -v "grep"| awk '{print $2}'`
+    echo "other times $IDD"
+    if [ ! "$IDD" ]
     then
         break
     fi
-    for id in $ID
-    do
-        kill $id
-        #IDD=`ps -ef | grep "cmd_meng.py" |grep -v "grep"| awk '{print $2}'`
-        #echo "programing $IDD"
-        echo "killed $id"
-    echo "sleep 10s..."
+    echo "other times sleep 10s..."
     sleep 10
-    done
 done
 
+echo "excute main program"
 nohup python /data1/spider/menggui/zhuanli_spider/cnipr/cmd_meng.py >> /data1/spider/menggui/zhuanli_spider/cnipr/meng_1.out 2>&1 &
