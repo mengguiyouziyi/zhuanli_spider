938203f26a486df19900030dbe9d0fd39a61b742
mengguiyouziyi <775618369@qq.com>
2017-11-28 13:24:34 +0800

    1

--- use/get_zhuanli.py
+++ use/get_zhuanli.py
@@ -220,7 +220,7 @@ def main():
 		id = result.get('id')
 		only_id = result.get('only_id')
 		proposer = result.get('comp_full_name')
-		if id < 787331:
+		if id < 828306:
 			continue
 		response = get_res(token, result, 1)
 		if response == -1:
